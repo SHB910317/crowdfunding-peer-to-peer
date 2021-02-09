9924850cb2e0ff0053828da49af50c27453622a4
SHB910317 <hernandezbarrero@gmail.com>
2021-02-09 13:03:13 -0500

    cambios de start

--- src/Views/Start.js
+++ src/Views/Start.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import logo from '../Assets/images/giving.svg';
+import { Button, Image } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 export default function Start() {
   let history = useHistory();
 
   return (
-    <div>
-      <button className='Home-btn' onClick= {() => { history.push('/signup') }}>signup</button>
-      <button className='Home-btn' onClick= {() => { history.push('/login') }}>login</button>
+    <div className="">
+     <h1> <Image src = {logo} roundedCircle/></h1> 
+
+     <h2>BIENVENIDO</h2>
+      <p>Ayudemos a miles ofreciendo tu razón como un momento para apoyar a quien más lo necesita.</p>
+        
+      <Button variant="primary" className='Home-btn' onClick= {() => { history.push('/signup') }}>signup</Button>
+      <Button variant="primary"className='Home-btn' onClick= {() => { history.push('/login') }}>login</Button>
     </div>
   )
 }
